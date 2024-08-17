86b76def7f7d68af6b03debd03036a1cf8e444d3
Bruce Zhu <brucezzhu@gmail.com>
2024-08-17 23:23:44 +1000

    upload

--- download.bat
+++ download.bat
@@ -1,13 +1,19 @@
 @echo off
 
-:: unzip the slave_1 folder, delete the original zip file, and move the folder to the given path
 
 git pull
 
 set "DEST_PATH=../"
 set "FOLDER_NAME=slave_1"
 
+:: Delete the folder if it already exists
+if exist "%DEST_PATH%%FOLDER_NAME%" (
+    echo Deleting existing "%DEST_PATH%%FOLDER_NAME%"...
+    rmdir /s /q "%DEST_PATH%%FOLDER_NAME%"
+)
+
+:: Move the folder to the destination path
+
 echo Moving "%FOLDER_NAME%" to "%DEST_PATH%"...
 move "%FOLDER_NAME%" "%DEST_PATH%"
 
-echo Done!
