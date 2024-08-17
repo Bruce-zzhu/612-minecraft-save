1ac2b081834e6233a9d400a46b9bdc50a04ec02f
Bruce Zhu <brucezzhu@gmail.com>
2024-08-17 23:27:35 +1000

    upload

--- upload.bat
+++ upload.bat
@@ -6,13 +6,10 @@ set "COMMIT_MESSAGE=upload"
 
 :: Copy the folder to the current directory
 echo Copying %FOLDER_NAME% to the current directory...
-xcopy /E /I "%FOLDER_PATH%\%FOLDER_NAME%" "%FOLDER_NAME%"
-
+xcopy /E /I /Y "%FOLDER_PATH%%FOLDER_NAME%" "%FOLDER_NAME%"
 
 :: Commit and push
 echo Committing and pushing to GitHub...
 git add .
 git commit -m "%COMMIT_MESSAGE%"
 git push
-
-echo Done!
