8d2e691b1edffbce1251481944767b1c0eb9b247
Bruce Zhu <brucezzhu@gmail.com>
2024-08-17 23:15:12 +1000

    upload

--- download.bat
+++ download.bat
@@ -1,3 +1,13 @@
 @echo off
 
+:: unzip the slave_1 folder, delete the original zip file, and move the folder to the given path
+
 git pull
+
+set "DEST_PATH=../"
+set "FOLDER_NAME=slave_1"
+
+echo Moving "%FOLDER_NAME%" to "%DEST_PATH%"...
+move "%FOLDER_NAME%" "%DEST_PATH%"
+
+echo Done!
--- upload.bat
+++ upload.bat
@@ -1,5 +1,18 @@
 @echo off
 
+set "FOLDER_PATH=../"
+set "FOLDER_NAME=slave_1"
+set "COMMIT_MESSAGE=upload"
+
+:: Copy the folder to the current directory
+echo Copying %FOLDER_NAME% to the current directory...
+xcopy /E /I "%FOLDER_PATH%\%FOLDER_NAME%" "%FOLDER_NAME%"
+
+
+:: Commit and push
+echo Committing and pushing to GitHub...
 git add .
-git commit -m "update"
+git commit -m "%COMMIT_MESSAGE%"
 git push
+
+echo Done!
